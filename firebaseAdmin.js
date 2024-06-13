db437806a5073966ccdf8111773c78e9e4cd069f
GabrielCorsiHonorio <gabrielcorsi@estudante.ufscar.br>
2024-06-13 16:36:26 -0300

    teste nova key

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -2,17 +2,28 @@
 const admin = require('firebase-admin');
 require('dotenv').config();
 
-let serviceAccount;
-try {
-  serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
-} catch (error) {
-  console.error('Error parsing FIREBASE_CONFIG:', error);
-  throw new Error('Invalid FIREBASE_CONFIG environment variable');
-}
+// let serviceAccount;
+// try {
+//   serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+// } catch (error) {
+//   console.error('Error parsing FIREBASE_CONFIG:', error);
+//   throw new Error('Invalid FIREBASE_CONFIG environment variable');
+// }
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      type: process.env.FIREBASE_TYPE,
+      project_id: process.env.FIREBASE_PROJECT_ID,
+      private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+      client_id: process.env.FIREBASE_CLIENT_ID,
+      auth_uri: process.env.FIREBASE_AUTH_URI,
+      token_uri: process.env.FIREBASE_TOKEN_URI,
+      auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+      client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    }),
     storageBucket: 'gchaparis.appspot.com'
   });
 }

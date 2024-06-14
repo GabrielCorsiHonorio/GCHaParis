0784181407972c283662748b2958a44cf05cd196
GabrielCorsiHonorio <gabrielcorsi@estudante.ufscar.br>
2024-06-14 13:45:52 -0300

    teste var 8

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,10 +1,13 @@
-// pages/api/users.js
 export default async function handler(req, res) {
+    console.log("Handler called with method:", req.method);
+  
     if (req.method === 'GET') {
       const usersJson = process.env.USERS_JSON;
   
       if (!usersJson) {
-        return res.status(500).json({ message: 'User data not available' });
+        console.error("USERS_JSON environment variable is not set");
+        res.status(500).json({ message: 'User data not available' });
+        return; // Adicione um return aqui
       }
   
       try {
@@ -12,11 +15,11 @@ export default async function handler(req, res) {
         res.status(200).json(users);
       } catch (error) {
         console.error("JSON parsing error:", error);
-        return res.status(500).json({ message: 'Error parsing user data' });
+        res.status(500).json({ message: 'Error parsing user data' });
       }
     } else {
+      console.error("Method not allowed:", req.method);
       res.status(405).json({ message: 'Method not allowed' });
     }
   }
-  
   

cbee43f835ea5dad8e967381cdbba99630d43e45
GabrielCorsiHonorio <gabrielcorsi@estudante.ufscar.br>
2024-06-13 23:06:29 -0300

    teste user json 2

--- pages/api/login.js
+++ pages/api/login.js
@@ -10,6 +10,8 @@ export default async function handler(req, res) {
     if (!usersJson) {
       return res.status(500).json({ message: 'User data not available' });
     }
+
+    try {
     const users = JSON.parse(usersJson);
 
     const user = users.find(user => user.username === username && user.password === password);
@@ -19,6 +21,10 @@ export default async function handler(req, res) {
     } else {
       res.status(401).json({ message: 'Invalid username or password' });
     }
+  }catch (error) {
+    // Capturar qualquer erro durante o parse do JSON
+    return res.status(500).json({ message: 'Error parsing user data' });
+  }
   } else {
     res.status(405).json({ message: 'Method not allowed' });
   }

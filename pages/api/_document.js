e2dff1d512824af936e4027ec222ff29632234ff
GabrielCorsiHonorio <gabrielcorsi@estudante.ufscar.br>
2024-08-04 15:19:13 +0200

    teste PWA

---
+++ pages/api/_document.js
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from "next/document"; 
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <head>
+                    <link rel="manifest" href="/manifest.json"/>
+                    {/* <link rel="icon-192x192" href="/public"/> */}
+                    <meta name="theme-color" content="#fff"/>
+                </head>
+                <body>
+                    <main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}

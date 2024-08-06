669bbb617e1c2d47f8dba0e8a17b4cefd6a42fcd
GabrielCorsiHonorio <gabrielcorsi@estudante.ufscar.br>
2024-08-06 20:49:43 +0200

    teste pwa2

--- pages/api/_document.js
+++ pages/api/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
             <Html>
                 <head>
                     <link rel="manifest" href="/manifest.json"/>
-                    {/* <link rel="icon-192x192" href="/public"/> */}
+                    <link rel="icon-192x192" href="/public"/>
                     <meta name="theme-color" content="#fff"/>
                 </head>
                 <body>
